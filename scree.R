aaf551cfb11ee33b6e89bcf610967c896996fe3a
yunxiaochen <y.chen186@lse.ac.uk>
2021-05-10 22:09:53 +0100

    Add files via upload

--- scree.R
+++ scree.R
@@ -2,9 +2,9 @@ N = 200
 J = 200
 
 sim1 <- function(N, J, K){
-  F.matr <- matrix(runif(N*K,-1,1), N, K);
+  F.matr <- matrix(runif(N*K,-2,2), N, K);
   F.matr <- cbind(rep(1,N), F.matr)
-  A.matr <- matrix(runif(J*(K+1),-1,1), J, K+1);                 
+  A.matr <- matrix(runif(J*(K+1),-2,2), J, K+1);                 
   
   temp = F.matr %*% t(A.matr);
   rate = exp(temp);
@@ -17,8 +17,8 @@ sim1 <- function(N, J, K){
 set.seed(1)
 data = sim1(N, J, 3)
 eigen.res = eigen(cov(data$resp))
-
+setwd("C:/Users/yxche/Dropbox (LSE Statistics)/Research/HighDim_ExpFac/JMLE_IC/revision/revision")
 pdf(file = "scree.pdf", height = 5, width = 5)
-plot(eigen.res$values[1:15], xlab = "eigenvalues", ylab = "eigenvalue")
+plot(eigen.res$values[1:15], xlab = "", ylab = "eigenvalue")
 
 dev.off()
